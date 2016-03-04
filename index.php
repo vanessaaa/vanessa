ab60b1db83d2c9b94dcbe26552debf12b4962ecc
vanessa choqque <vaneytha@gmail.com>
2016-03-04 00:02:29 -0500

    primer proyecto

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet"href="estilos.css">
+	<body>
+		    <div class= "contenedor">
+		        <nav>
+		        	<ul class="menu-lista">
+		        		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+		        		<li class="menu-item"><a href="" class="menu-link">CONTACTANOS</a></li>
+		        	</ul>
+		        </nav>
+		    </div class= "slider">
+		    <img src="3.jpg" heigth="10" width="1350">
+		    <img src="4.jpg.lnk" heigth="200" width="330">
+		    <img src="5.jpg.lnk" heigth="200" width="330">
+		    <img src="6.jpg.lnk" heigth="200" width="330">
+		    <img src="7.jpg.lnk" heigth="200" width="330">
+		    <h1>Hay una pequeña diferencia en las personas, pero esa diferencia marca una gran diferencia. La pequeña diferencia es la actitud. La gran diferencia es si es positiva o negativa.-W. Clement Stone</h1>
+
+		</body>
+		</html>
